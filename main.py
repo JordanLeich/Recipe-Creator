d430ec2250a5e8741ab8005b7a0c8dc54b58479d
Jordan Leich <jordanleich@gmail.com>
2022-01-18 13:51:37 -0500

    greatly improved the recipe coding and reduced alot of code, updated exe version.

--- main.py
+++ main.py
@@ -8,89 +8,61 @@ def main():  # sourcery no-metrics
     """Holds all possible recipe ingredients and then outputs all possible recipes based upon every line that is read
     in the ingredients.txt file."""
     ingredients_list_file_path = 'ingredients.txt'
+
     try:
         with open('ingredients.txt') as file:
             ingredients_list = file.read().splitlines()
-            if 'milk' in ingredients_list and 'noodles' in ingredients_list and 'cheese' in ingredients_list:
-                print_green('Macaroni & Cheese\n')
-            if 'cheese' in ingredients_list and 'dough' in ingredients_list:
-                print_green('Cheese Sticks\n')
-                print_green('Cheese Bread\n')
-            if 'bread' in ingredients_list:
-                print_green('Baked Bread Sticks\n')
-            if 'tortilla' in ingredients_list and 'cheese' in ingredients_list:
-                print_green('Cheese Quesadillas\n')
-            if 'tortilla' in ingredients_list and 'cheese' in ingredients_list and 'chicken' in ingredients_list:
-                print_green('Chicken & Cheese Quesadillas\n')
-            if 'tortilla' in ingredients_list and 'cheese' in ingredients_list and 'meat' in ingredients_list:
-                print_green('Meat & Cheese Quesadillas\n')
-            if 'cheese' in ingredients_list and 'bread' in ingredients_list and 'ham' in ingredients_list:
-                print_green('Ham & Cheese Sandwich\n')
-            if 'eggs' in ingredients_list or 'egg' in ingredients_list:
-                print_green('Scrambled Eggs\n')
-                print_green('Sunny Side Eggs\n')
-                print_green('Omelette\n')
-            if 'bread' in ingredients_list and 'hotdog' in ingredients_list or 'bun' in ingredients_list:
-                print_green('Hotdogs\n')
-            if 'meat' in ingredients_list and 'shell' in ingredients_list:
-                print_green('Tacos w/Meat\n')
-            if 'chicken' in ingredients_list and 'shell' in ingredients_list or 'shells' in \
-                    ingredients_list:
-                print_green('Tacos w/Chicken\n')
-            if 'meat' in ingredients_list and 'chips' in ingredients_list or 'shells' in \
-                    ingredients_list and 'salad' in ingredients_list or 'lettuce' in ingredients_list:
-                print_green('Tacos Salad\n')
-            if 'bread' in ingredients_list and 'cheese' in ingredients_list:
-                print_green('Grilled Cheese\n')
-            if 'bread' in ingredients_list and 'egg' in ingredients_list or 'eggs' in ingredients_list:
-                print_green('French Toast\n')
-            if 'bread' in ingredients_list and 'meat' in ingredients_list or 'burger' in ingredients_list:
-                print_green('Burger\n')
-            if 'bread' in ingredients_list and 'meat' in ingredients_list or 'burger' in ingredients_list and 'cheese' \
-                    in ingredients_list:
-                print_green('Cheese Burger\n')
-            if 'noodles' in ingredients_list and 'sauce' in ingredients_list:
-                print_green('Pasta\n')
-                print_green('Spaghetti\n')
-                print_green('Alfredo\n')
-            if 'noodles' in ingredients_list and 'sauce' in ingredients_list and 'meat' in ingredients_list:
-                print_green('Pasta or Spaghetti or Alfredo w/Meat\n')
-            if 'noodles' in ingredients_list and 'sauce' in ingredients_list and 'chicken' in ingredients_list:
-                print_green('Pasta or Spaghetti or Alfredo w/Chicken\n')
-            if 'chicken' in ingredients_list and 'rice' in ingredients_list:
-                print_green('Chicken & Rice Combination\n')
-            if 'ice cream' in ingredients_list and 'milk' in ingredients_list:
-                print_green('Ice Cream Shake Drink\n')
-            if 'potato' in ingredients_list or 'potatoes' in ingredients_list:
-                print_green('Baked potato\n')
-                print_green('French Fries\n')
-            if 'potato' in ingredients_list or 'potatoes' in ingredients_list and 'cheese' in ingredients_list:
-                print_green('Cheesy Baked potato\n')
-                print_green('French Fries\n')
-            if 'meat' in ingredients_list and 'tomato' in ingredients_list or 'tomatoes' in ingredients_list:
-                print_green('Chili\n')
-            if 'beans' in ingredients_list and 'meat' in ingredients_list and 'tomato' in ingredients_list or \
-                    'tomatoes' in ingredients_list:
-                print_green('Chili w/Beans\n')
-            if 'potato' in ingredients_list or 'potatoes' in ingredients_list and 'cheese' in ingredients_list and \
-                    'meat' in ingredients_list or 'chili' in ingredients_list:
-                print_green('Chili Cheese French Fries\n')
-            if 'dough' in ingredients_list and 'cheese' in ingredients_list and 'sauce' in ingredients_list:
-                print_green('Pizza\n')
-            if 'dough' in ingredients_list and 'cheese' in ingredients_list and 'sauce' in ingredients_list and 'meat' \
-                    in ingredients_list:
-                print_green('Pizza w/Meat\n')
-            if 'dough' in ingredients_list and 'cheese' in ingredients_list and 'sauce' in ingredients_list and \
-                    'chicken' in ingredients_list:
-                print_green('Pizza w/Chicken\n')
-            if 'chips' in ingredients_list and 'meat' in ingredients_list and 'salsa' in ingredients_list:
-                print_green('Nachos\n')
-            if 'chips' in ingredients_list and 'meat' in ingredients_list and 'salsa' in ingredients_list and \
-                    'cheese' in ingredients_list:
-                print_green('Nachos w/Cheese\n')
+
+            recipes = {  # TODO: Keep adding more recipes
+                'Macaroni & Cheese': ['milk', 'noodles', 'cheese'],
+                'Cheese Sticks': ['cheese', 'dough'],
+                'Cheese Bread': ['cheese', 'dough'],
+                'Baked Bread Sticks': ['bread'],
+                'Cheese Quesadillas': ['tortilla', 'cheese'],
+                'Chicken & Cheese Quesadillas': ['chicken', 'cheese', 'tortilla'],
+                'Meat & Cheese Quesadillas': ['meat', 'cheese', 'tortilla'],
+                'Ham & Cheese Sandwich': ['ham', 'cheese', 'bread'],
+                'Scrambled Eggs': ['eggs'],
+                'Sunny Side Eggs': ['eggs'],
+                'Omelette': ['eggs'],
+                'Hotdogs': ['buns', 'hotdog'],
+                'Chili Cheese Dogs': ['buns', 'hotdog', 'chili', 'cheese'],
+                'Tacos': ['meat', 'shell'],
+                'Tacos w/Chicken': ['chicken', 'shell'],
+                'Taco Salad': ['meat', 'chips', 'lettuce'],
+                'Grilled Cheese': ['cheese', 'bread'],
+                'French Toast': ['bread', 'eggs'],
+                'Burger': ['meat', 'buns'],
+                'Cheese Burger': ['meat', 'buns', 'cheese'],
+                'Pasta': ['noodles', 'sauce'],
+                'Pasta w/Meat': ['noodles', 'sauce', 'meat'],
+                'Spaghetti': ['noodles', 'sauce'],
+                'Alfredo': ['noodles', 'sauce'],
+                'Chicken Alfredo': ['noodles', 'sauce', 'chicken'],
+                'Chicken & Rice Combination': ['chicken', 'rice'],
+                'Ice Cream Shake Drink': ['ice cream', 'milk'],
+                'Baked potato': ['potatoes'],
+                'French Fries': ['potatoes'],
+                'Cheesy Baked potato': ['potatoes', 'cheese'],
+                'Chili': ['meat', 'tomatoes'],
+                'Chili Cheese French Fries': ['meat', 'tomatoes', 'potatoes', 'cheese'],
+                'Chili w/Beans': ['meat', 'tomatoes', 'beans'],
+                'Pizza': ['dough', 'sauce', 'cheese'],
+                'Pepperoni Pizza': ['dough', 'sauce', 'cheese', 'pepperoni'],
+                'Pizza w/Meat': ['dough', 'sauce', 'cheese', 'meat'],
+                'Pizza w/Chicken': ['dough', 'sauce', 'cheese', 'chicken'],
+                'Nachos': ['chips', 'meat', 'salsa'],
+                'Nachos w/Cheese': ['chips', 'meat', 'salsa', 'cheese'],
+            }
+
+            for food, ingredients in recipes.items():
+                if all(map(lambda i: i in ingredients_list, ingredients)):
+                    print_green(food)
+                    print()
             if os.stat(ingredients_list_file_path).st_size == 0:
                 print_red('No recipes can be created, try adding more to the ingredients.txt file and rerun the '
                           'program!\n', 3)
+
         sleep(1)
         input("Press enter to exit...")
         file.close()
