1a854ef1f5ce42d37300f6af186360e185719917
Jordan Leich <jordanleich@gmail.com>
2023-10-18 22:38:50 -0400

    major improvements

--- README.md
+++ README.md
@@ -7,8 +7,7 @@
     <img src="https://img.shields.io/github/contributors/jordanleich/Recipe-Creator">
     <img src="https://img.shields.io/badge/-Cooking-brightgreen">    
     <img src="https://img.shields.io/github/languages/code-size/JordanLeich/Recipe-Creator">    
-    <img src="https://img.shields.io/github/repo-size/JordanLeich/Recipe-Creator"> 
-    <img src="https://img.shields.io/tokei/lines/github/JordanLeich/Recipe-Creator">
+    <img src="https://img.shields.io/github/repo-size/JordanLeich/Recipe-Creator">
     <img src="https://img.shields.io/github/stars/jordanleich/Recipe-Creator?style=socialhttps://img.shields.io/tokei/lines/github/JordanLeich/Recipe-Creator?label=lines%20of%20code"> 
     <img src="https://img.shields.io/github/stars/jordanleich?label=user%20stars&style=social"> 
     <img src="https://img.shields.io/github/v/release/jordanleich/Recipe-Creator?include_prereleases"> 
@@ -16,15 +15,12 @@
 </p>
 
 # Description 🍕
-- A simple python script where the user inputs the current ingredients they have in their kitchen into ingredients.txt 
-and then runs the main.py script, and it will output what recipes can be created based upon the ingredients supported.
 
-# Important ⚡
-- Certain foods and ingredients like non-sticks (butters or oils), frozen foods, seasonings, and condiments are not supported in the ingredients.txt file. 
-For example, if you have bread and eggs, it will be assumed you also have cinnamon, and thus you can make french toast.
+- A simple python script where the user inputs the current ingredients they have in their kitchen into ingredients.txt
+  and then runs the main.py script, and it will output what recipes can be created based upon the ingredients supported.
+- Also includes an .exe version for those who do not have Python installed.
 
 # Examples 🍕
+
 ![Ingredients](examples/ingredients.PNG "Ingredients")
 ![Recipes](examples/recipes.PNG "Recipes")
-
-# [Requirements](https://github.com/JordanLeich/Recipe-Creator/blob/main/requirements.txt) ⚡
--- main.py
+++ main.py
@@ -1,133 +1,178 @@
 import sys
-from colors import print_red, print_green, print_yellow
-from os.path import exists as file_exists
+import os
 
 # Constant variables
 INGREDIENTS_LIST_FILE_PATH = 'ingredients.txt'
 
+recipes = [
+        ('Macaroni & Cheese', ['milk', 'noodles', 'cheese']),
+        ('Cheese Sticks', ['cheese', 'dough']),
+        ('Cheese Bread', ['cheese', 'dough']),
+        ('Baked Bread Sticks', ['bread']),
+        ('Cheese Quesadillas', ['tortilla', 'cheese']),
+        ('Chicken & Cheese Quesadillas', ['chicken', 'cheese', 'tortilla']),
+        ('Meat & Cheese Quesadillas', ['meat', 'cheese', 'tortilla']),
+        ('Ham & Cheese Sandwich', ['ham', 'cheese', 'bread']),
+        ('Bacon & Cheese Sandwich', ['bacon', 'cheese', 'bread']),
+        ('BLT Sandwich', ['bacon', 'tomatoes', 'bread', 'lettuce']),
+        ('Fish Sandwich', ['fish', 'bread']),
+        ('Turkey Sandwich', ['turkey', 'bread']),
+        ('Turkey Sandwich w/Cheese', ['turkey', 'bread', 'cheese']),
+        ('Sausage & Egg Muffin', ['sausage', 'eggs', 'muffins']),
+        ('Scrambled Eggs', ['eggs']),
+        ('Cheesy Scrambled Eggs', ['eggs', 'cheese']),
+        ('Sunny Side Eggs', ['eggs']),
+        ('Omelette', ['eggs']),
+        ('Hot Dogs', ['buns', 'hotdog']),
+        ('Chili Cheese Dogs', ['buns', 'hotdog', 'chili', 'cheese']),
+        ('Tacos', ['meat', 'shell']),
+        ('Tacos w/Chicken', ['chicken', 'shell']),
+        ('Taco Salad', ['meat', 'chips', 'lettuce']),
+        ('Grilled Chicken Salad', ['lettuce', 'chicken']),
+        ('Grilled Chicken Avocado Salad', ['lettuce', 'chicken', 'avocado']),
+        ('Grilled Cheese', ['cheese', 'bread']),
+        ('French Toast', ['bread', 'eggs']),
+        ('Burger', ['meat', 'buns']),
+        ('Cheese Burger', ['meat', 'buns', 'cheese']),
+        ('Pasta', ['noodles', 'sauce']),
+        ('Pasta w/Meat', ['noodles', 'sauce', 'meat']),
+        ('Spaghetti', ['noodles', 'sauce']),
+        ('Alfredo', ['noodles', 'sauce']),
+        ('Chicken Alfredo', ['noodles', 'sauce', 'chicken']),
+        ('Shrimp Scampi Linguine', ['shrimp', 'sauce', 'noodles']),
+        ('Chicken & Rice Combination', ['chicken', 'rice']),
+        ('Ice Cream Shake Drink', ['ice cream', 'milk']),
+        ('Baked potato', ['potatoes']),
+        ('French Fries', ['potatoes']),
+        ('Cheesy Baked potato', ['potatoes', 'cheese']),
+        ('Chili', ['meat', 'tomatoes']),
+        ('Chili Cheese French Fries', ['meat', 'tomatoes', 'potatoes', 'cheese']),
+        ('Chili w/Beans', ['meat', 'tomatoes', 'beans']),
+        ('Pizza', ['dough', 'sauce', 'cheese']),
+        ('Pepperoni Pizza', ['dough', 'sauce', 'cheese', 'pepperoni']),
+        ('Pizza w/Meat', ['dough', 'sauce', 'cheese', 'meat']),
+        ('Pizza w/Chicken', ['dough', 'sauce', 'cheese', 'chicken']),
+        ('Nachos w/Meat', ['chips', 'meat', 'salsa']),
+        ('Nachos w/Chicken', ['chips', 'chicken', 'salsa']),
+        ('Nachos w/Cheese', ['chips', 'meat', 'salsa', 'cheese']),
+        ('Ice Cream Sandwiches', ['ice cream', 'crackers']),
+        ('Smore Sandwiches', ['chocolate', 'crackers', 'marshmallows']),
+        ('Fruit Salad', ['apple', 'banana', 'orange', 'grapes']),
+        ('Fruit Smoothie', ['banana', 'strawberries', 'yogurt']),
+        ('Caesar Salad', ['lettuce', 'croutons', 'parmesan', 'dressing']),
+        ('Caprese Salad', ['tomatoes', 'mozzarella', 'basil', 'olive oil']),
+        ('Grilled Shrimp', ['shrimp', 'garlic', 'lemon', 'butter']),
+        ('Margarita Pizza', ['dough', 'tomatoes', 'mozzarella', 'basil', 'olive oil']),
+        ('Vegetable Stir-Fry', ['broccoli', 'carrots', 'bell pepper', 'zucchini', 'soy sauce']),
+        ('Spicy Tofu Curry', ['tofu', 'coconut milk', 'curry paste', 'bell pepper', 'onion']),
+        ('Homemade Lasagna', ['lasagna noodles', 'ground beef', 'ricotta cheese', 'mozzarella cheese', 'tomato sauce']),
+        ('Sushi Rolls', ['sushi rice', 'nori seaweed', 'fish', 'avocado', 'cucumber', 'soy sauce']),
+        ('Spinach and Feta Stuffed Chicken', ['chicken breast', 'spinach', 'feta cheese', 'garlic', 'lemon']),
+        ('Mango Salsa', ['mango', 'red onion', 'cilantro', 'lime juice']),
+        ('Avocado Toast', ['avocado', 'bread', 'salt', 'pepper', 'red pepper flakes']),
+        ('Chocolate Chip Cookies', ['flour', 'butter', 'sugar', 'chocolate chips', 'baking soda']),
+        ('Fruit Smoothie Bowl', ['yogurt', 'banana', 'strawberries', 'granola', 'honey']),
+        ('Pancakes', ['flour', 'milk', 'eggs', 'baking powder', 'syrup']),
+        ('Greek Salad', ['cucumber', 'tomato', 'red onion', 'kalamata olives', 'feta cheese']),
+        ('Hawaiian Pizza', ['dough', 'tomato sauce', 'ham', 'pineapple', 'mozzarella cheese']),
+        ('Crispy Chicken Tenders', ['chicken tenders', 'flour', 'eggs', 'bread crumbs', 'spices']),
+        ('Garlic Butter Shrimp', ['shrimp', 'butter', 'garlic', 'lemon juice', 'parsley']),
+        ('Caprese Pasta Salad', ['pasta', 'tomatoes', 'mozzarella cheese', 'basil', 'balsamic dressing']),
+        ('Veggie Burger', ['veggie patty', 'buns', 'lettuce', 'tomato', 'onion', 'mayonnaise']),
+        ('Lemonade', ['lemons', 'sugar', 'water']),
+        ('Cucumber Dill Salad', ['cucumbers', 'dill', 'onion', 'sour cream']),
+        ('Tofu Scramble', ['tofu', 'bell pepper', 'onion', 'turmeric', 'spinach']),
+        ('Cauliflower Rice', ['cauliflower', 'olive oil', 'garlic', 'spices']),
+        ('Pulled Pork Sandwich', ['pork shoulder', 'buns', 'barbecue sauce', 'coleslaw']),
+        ('Shrimp and Grits', ['shrimp', 'grits', 'bacon', 'cheese', 'green onions']),
+        ('Ratatouille', ['eggplant', 'zucchini', 'tomatoes', 'bell pepper', 'garlic']),
+        ('Stuffed Bell Peppers', ['bell peppers', 'ground beef', 'rice', 'tomato sauce', 'onion']),
+        ('Garlic Knots', ['pizza dough', 'butter', 'garlic', 'parsley']),
+        ('Peanut Butter and Jelly Sandwich', ['bread', 'peanut butter', 'jelly']),
+        ('Scrambled Tofu', ['tofu', 'turmeric', 'black salt', 'pepper']),
+        ('Vegetable Omelette', ['eggs', 'bell pepper', 'onion', 'mushrooms', 'cheese']),
+        ('Pasta with Garlic and Olive Oil', ['spaghetti', 'garlic', 'olive oil', 'red pepper flakes']),
+        ('Cereal with Milk', ['cereal', 'milk']),
+        ('Fried Rice', ['cooked rice', 'vegetables', 'soy sauce', 'eggs']),
+        ('Tomato Soup', ['tomatoes', 'onion', 'garlic', 'vegetable broth', 'cream']),
+        ('Mashed Potatoes', ['potatoes', 'butter', 'milk']),
+        ('Classic BLT Sandwich', ['bread', 'bacon', 'lettuce', 'tomato']),
+        ('Oatmeal', ['oats', 'water', 'milk', 'sugar', 'cinnamon']),
+        ('Chicken Noodle Soup', ['chicken broth', 'chicken', 'noodles', 'carrots', 'celery']),
+        ('Greek Salad', ['cucumber', 'tomato', 'red onion', 'kalamata olives', 'feta cheese']),
+    ]
 
-def add_to_ingredients_file() -> list:
+def add_ingredients_to_file():
     """Allows the user to manually add ingredients to their ingredients.txt file."""
-    print_yellow('Please enter either exit, close, or stop to stop adding ingredients to your list.\n', 1)
+    print('Please enter the ingredients. Enter "exit" to stop.')
 
-    with open(INGREDIENTS_LIST_FILE_PATH, 'w') as file:
-        ingredients_list = []
-        user_input_ingredients = ''
-        while user_input_ingredients.lower() not in ['exit', 'close', 'stop']:
-            user_input_ingredients = str(input('Ingredient name: '))
-            print()
-            file.write(f"{user_input_ingredients}\n")
-            ingredients_list.append(user_input_ingredients)
-            print_green('Ingredient added!\n')
-        print_yellow('Ingredients have finished being added.\n', 1)
-    print_green('Here are all of the possible recipes.\n', 1)
+    ingredients_list = []
+    with open(INGREDIENTS_LIST_FILE_PATH, 'a') as file:
+        while True:
+            user_input_ingredients = input('Ingredient name: ').strip()
+            if user_input_ingredients.lower() in ['exit', 'close', 'stop']:
+                break
+            if user_input_ingredients:
+                file.write(f"{user_input_ingredients}\n")
+                ingredients_list.append(user_input_ingredients)
+                print('Ingredient added!')
+            else:
+                print('Invalid ingredient name. Please try again.')
+
+    print('Ingredients have finished being added.\n')
     return ingredients_list
 
-
-def main() -> None:  # sourcery no-metrics
-    """Holds all possible recipe ingredients and then outputs all possible recipes based upon every line that is read
-    in the ingredients.txt file."""
-
-    if file_exists(INGREDIENTS_LIST_FILE_PATH):  # To verify the file exist.
-        with open(INGREDIENTS_LIST_FILE_PATH) as file:
+def load_ingredients_from_file():
+    if os.path.exists(INGREDIENTS_LIST_FILE_PATH):
+        with open(INGREDIENTS_LIST_FILE_PATH, 'r') as file:
             ingredients_list = file.read().splitlines()
-    else:  # When the file doesn't exist.
-        print_red('Since the ingredients.txt file does not exist, a new file will be created!\n', 2)
-        ingredients_list = add_to_ingredients_file()
+            return ingredients_list
+    else:
+        return []
 
-    recipes = {  # TODO: Keep adding more recipes
-        'Macaroni & Cheese': ['milk', 'noodles', 'cheese'],
-        'Cheese Sticks': ['cheese', 'dough'],
-        'Cheese Bread': ['cheese', 'dough'],
-        'Baked Bread Sticks': ['bread'],
-        'Cheese Quesadillas': ['tortilla', 'cheese'],
-        'Chicken & Cheese Quesadillas': ['chicken', 'cheese', 'tortilla'],
-        'Meat & Cheese Quesadillas': ['meat', 'cheese', 'tortilla'],
-        'Ham & Cheese Sandwich': ['ham', 'cheese', 'bread'],
-        'Bacon & Cheese Sandwich': ['bacon', 'cheese', 'bread'],
-        'BLT Sandwich': ['bacon', 'tomatoes', 'bread', 'lettuce'],
-        'Fish Sandwich': ['fish', 'bread'],
-        'Turkey Sandwich': ['turkey', 'bread'],
-        'Turkey Sandwich w/Cheese': ['turkey', 'bread', 'cheese'],
-        'Sausage & Egg Muffin': ['sausage', 'eggs', 'muffins'],
-        'Scrambled Eggs': ['eggs'],
-        'Cheesy Scrambled Eggs': ['eggs', 'cheese'],
-        'Sunny Side Eggs': ['eggs'],
-        'Omelette': ['eggs'],
-        'Hot Dogs': ['buns', 'hotdog'],
-        'Chili Cheese Dogs': ['buns', 'hotdog', 'chili', 'cheese'],
-        'Tacos': ['meat', 'shell'],
-        'Tacos w/Chicken': ['chicken', 'shell'],
-        'Taco Salad': ['meat', 'chips', 'lettuce'],
-        'Grilled Chicken Salad': ['lettuce', 'chicken'],
-        'Grilled Chicken Avocado Salad': ['lettuce', 'chicken', 'avocado'],
-        'Grilled Cheese': ['cheese', 'bread'],
-        'French Toast': ['bread', 'eggs'],
-        'Burger': ['meat', 'buns'],
-        'Cheese Burger': ['meat', 'buns', 'cheese'],
-        'Pasta': ['noodles', 'sauce'],
-        'Pasta w/Meat': ['noodles', 'sauce', 'meat'],
-        'Spaghetti': ['noodles', 'sauce'],
-        'Alfredo': ['noodles', 'sauce'],
-        'Chicken Alfredo': ['noodles', 'sauce', 'chicken'],
-        'Shrimp Scampi Linguine': ['shrimp', 'sauce', 'noodles'],
-        'Chicken & Rice Combination': ['chicken', 'rice'],
-        'Ice Cream Shake Drink': ['ice cream', 'milk'],
-        'Baked potato': ['potatoes'],
-        'French Fries': ['potatoes'],
-        'Cheesy Baked potato': ['potatoes', 'cheese'],
-        'Chili': ['meat', 'tomatoes'],
-        'Chili Cheese French Fries': ['meat', 'tomatoes', 'potatoes', 'cheese'],
-        'Chili w/Beans': ['meat', 'tomatoes', 'beans'],
-        'Pizza': ['dough', 'sauce', 'cheese'],
-        'Pepperoni Pizza': ['dough', 'sauce', 'cheese', 'pepperoni'],
-        'Pizza w/Meat': ['dough', 'sauce', 'cheese', 'meat'],
-        'Pizza w/Chicken': ['dough', 'sauce', 'cheese', 'chicken'],
-        'Nachos w/Meat': ['chips', 'meat', 'salsa'],
-        'Nachos w/Chicken': ['chips', 'chicken', 'salsa'],
-        'Nachos w/Cheese': ['chips', 'meat', 'salsa', 'cheese'],
-        'Ice Cream Sandwiches': ['ice cream', 'crackers'],
-        'Smore Sandwiches': ['chocolate', 'crackers', 'marshmallows'],
-    }
+def find_recipes(ingredients_list):
     found_food = False
-    for food, ingredients in recipes.items():
-        if all(x in ingredients_list for x in ingredients):  # check if ingredients are a sublist of ingredients_list
-            print_green(food)
+    for food, recipe_ingredients in recipes:
+        if all(ingredient.lower() in map(str.lower, ingredients_list) for ingredient in recipe_ingredients):
+            print(food)
             found_food = True
-            print()
-    choice = input('Press enter to either continue or exit. Enter add to add to your ingredients list. '
-                   'Enter clear to clear all ingredients from the ingredients file: ')
-    print()
 
-    if choice.lower() in ['add', 'a']:
-        add_to_ingredients_file()
-        main()
+    return found_food
 
-    if choice.lower() in ['clear', 'clr', 'c']:
-        with open(INGREDIENTS_LIST_FILE_PATH, 'r+') as file:
-            file.truncate(0)
-            print_green('Ingredients file as been cleared successfully!\n', 1)
-            choice = input('Want to add to your ingredients list? (yes / no): ')
-            print()
-            if choice.lower() in ['yes', 'y']:
-                add_to_ingredients_file()
-                main()
-            else:
-                sys.exit()
+def clear_ingredients_file():
+    try:
+        os.remove(INGREDIENTS_LIST_FILE_PATH)
+        print('Ingredients file has been cleared successfully.')
+    except FileNotFoundError:
+        print('Ingredients file not found.')
 
-    if not found_food:
-        print_red('Sorry, no recipes can be created.\n')
-        user_input = str(input('Would you like to add ingredients to the ingredients.txt file (yes / no): '))
-        print()
-        if user_input.lower() in ['yes', 'y', 'sure']:
-            add_to_ingredients_file()
+def get_all_possible_ingredients():
+    all_possible_ingredients = list(set(ingredient for recipe in recipes for ingredient in recipe[1]))
+    return all_possible_ingredients
+
+def main():
+    while True:
+        ingredients_list = load_ingredients_from_file()
+        if not ingredients_list:
+            print('Ingredients list is empty. Let\'s add some ingredients.')
+            ingredients_list = add_ingredients_to_file()
+
+        found_food = find_recipes(ingredients_list)
+
+        choice = input('Press Enter to exit, enter "add" to add more ingredients, "clear" to clear the list, or "list" to view all ingredients: ')
+        if choice.lower() == 'add':
+            ingredients_list = add_ingredients_to_file()
+        elif choice.lower() == 'clear':
+            clear_ingredients_file()
+        elif choice.lower() == 'list':
+            all_possible_ingredients = get_all_possible_ingredients()
+            print('List of All Possible Ingredients:')
+            for i, ingredient in enumerate(all_possible_ingredients, start=1):
+                print(f"{i}. {ingredient}")
         else:
+            if not found_food:
+                print('Sorry, no recipes can be created.')
             sys.exit()
 
-    if found_food:
-        sys.exit()
-
-
 if __name__ == '__main__':
-    while True:
-        main()
+    main()
